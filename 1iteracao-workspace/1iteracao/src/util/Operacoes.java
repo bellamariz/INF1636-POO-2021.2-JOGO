be0d87298339ffb05d608f9bfe5765822f7bef62
Rachel <rachel.szenberg@gmail.com>
2021-11-28 12:11:37 -0300

    operacoes definidas

--- 1iteracao-workspace/1iteracao/src/util/Operacoes.java
+++ 1iteracao-workspace/1iteracao/src/util/Operacoes.java
@@ -3,5 +3,9 @@ package util;
 public class Operacoes {
     public static final int OPERACAO_OLA = 1;
     public static final int OPERACAO_TCHAU = 2;
-    public static final int DADO_LANCADO = 3;
+    public static final int ORDENA_JOGADORES = 3; //mostra o dado lancado para ordenar os jogadores
+    public static final int DADO_LANCADO = 4; //apertou o botao lancar dado
+    public static final int MOVE_EXPLORADOR = 5; //selecionou o explorador e a casa que quer que ele va
+    public static final int MOVIMENTO_CARTA = 6; //retirou a carta 1 ou 3 e vai fazer a movimentacao relacionada a elas
+    public static final int CONTA_PONTOS = 7; //chamada no fim do jogo
 }
